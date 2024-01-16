389a482716695e732e01549f5462b01b30a3747b
NikiNickie <156077173+NikiNickie@users.noreply.github.com>
2024-01-16 11:34:55 +0100

    Update kaartenV2.py

--- kaartenV2.py
+++ kaartenV2.py
@@ -1,18 +1,18 @@
 import random
-class kaarten:
-   def __init__(self,number,symbool,shading,colour):
+class cards:
+   def __init__(self,colour,symbol,shading,number):
       self._number=number
-      self._symbool=symbool
+      self._symbol=symbol
       self._shading=shading
       self._colour=colour
    def __repr__(self):
-      return self.number + self.symbool + self.shading + self.colour
+      return self.number + self.symbol + self.shading + self.colour
    @property
    def number(self):
       return self._number
    @property
    def symbool(self):
-      return self._symbool
+      return self._symbol
    @property
    def shading(self):
       return self._shading
@@ -28,7 +28,7 @@ class kaarten:
    @symbool.setter
    def symbool(self,symbool):
       if symbool in ["diamond","wave","oval"]:
-         self._symbool=symbool
+         self._symbol=symbol
       else:
          print ("That's not a symbool")
    @shading.setter
@@ -48,21 +48,21 @@ class Deck:
    
       
    def __init__(self):
-     self._kaarten=[]
+     self._cards=[]
      self.populate()
-     print(self._kaarten)
+     print(self._cards)
    def __len__(self):
-      return len(self._kaarten)
+      return len(self._cards)
    def populate(self):
       number=["1","2","3"]
       symbool=["diamond","wave","oval"]
       shading=["filled","empty","stripped"]
       colour=["green","purple","red"]
-      self._kaarten=[kaarten(a,b,c,d) for a in number for b in symbool for c in shading for d in colour ]
-      return self._kaarten
+      self._cards=[cards(a,b,c,d) for a in number for b in symbool for c in shading for d in colour ]
+      return self._cards
    def shuffle(self):
-      for k in range(len(self._kaarten)-1,0,-1):
-         random.shuffle(self._kaarten)
+      for k in range(len(self._cards)-1,0,-1):
+         random.shuffle(self._cards)
       print(my_deck)
    
       
