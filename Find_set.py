20720910b30f35ceb09f90047e02de1bea61881c
Quinn2411 <156076473+Quinn2411@users.noreply.github.com>
2024-01-18 14:49:25 +0100

    Increase efficiency Find_set.py

--- Find_set.py
+++ Find_set.py
@@ -4,14 +4,14 @@ from Card import cards, Deck
 #Writing a function to find sets
 def findset(table):
     setsontable = []    
-    for card1 in table:
-        for card2 in table:
-            for card3 in table:
+    for i, card1 in enumerate(table):
+        for j, card2 in enumerate(table[i:]):
+            for k, card3 in enumerate(table[j:]):
                 if card1 != card2 and card2 != card3 and card1 != card3:
                     if check_set(card1, card2, card3) == True: 
                         #Returns set to a list of all sets on the table.
                         setsontable.append([card1, card2, card3])
-                        break
+                        
     return setsontable
 
 table = [cards("1","diamond", "filled", "purple"),
@@ -20,3 +20,4 @@ cards("1","diamond", "filled", "green")]
 
 setsontable = findset(table)
 print(setsontable)
+
