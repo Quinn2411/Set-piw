baf46a637e484eb4d38e49741022528ba171dab2
Quinn2411 <156076473+Quinn2411@users.noreply.github.com>
2024-01-31 23:37:38 +0100

    Update find_one_set.py

--- find_one_set.py
+++ find_one_set.py
@@ -2,7 +2,7 @@ from check_set import check_set
 
 # writing a function to find one set on the table
 def find_one_set(A):
-    # making use of enumerate to skip checking duplicates (if card1, card2, card3 is checked we dont check card1, card3 ,card2 aswell)
+    # making use of enumerate to skip checking duplicates (if card1, card2, card3 is checked we dont check card1, card3 , card2 aswell)
     for i, card1 in enumerate(A._table): 
         for j, card2 in enumerate(A._table[i:]):
             for k, card3 in enumerate(A._table[j:]):
