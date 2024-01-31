bc20a849288a5d805ce1fa6a3e48f0f876137ad7
Quinn2411 <156076473+Quinn2411@users.noreply.github.com>
2024-01-31 20:49:24 +0100

    Cleaner Check_set.py

--- Check_Set.py
+++ Check_Set.py
@@ -1,17 +1,11 @@
-
-
-
-
-
-
-
 #Comparing all elements of the cards
-def check_set():
-    number_check = checkelement (Card1.number, Card2.number, Card3.number)
+def check_set(Card1, Card2, Card3):
+    colour_check = checkelement (Card1.colour, Card2.colour, Card3.colour)
     symbol_check = checkelement (Card1.symbol, Card2.symbol, Card3.symbol)
     shading_check = checkelement (Card1.shading, Card2.shading, Card3.shading)
-    colour_check = checkelement (Card1.colour, Card2.colour, Card3.colour)
-    return colour_check and symbol_check and shading_check and number_check
+    number_check = checkelement (Card1.number, Card2.number, Card3.number)
+    return colour_check and symbol_check and number_check and shading_check
+        
 
 #To check if a specific dimension of the card is allowed to form a set
 def checkelement(property1, property2, property3):
@@ -20,3 +14,4 @@ def checkelement(property1, property2, property3):
     elif property1 != property2 and property2 != property3 and property1 != property3:
         return True
     return False
+
