9e8d1ab7bc2079fe54395736c86b17f3222526b5
Quinn2411 <156076473+Quinn2411@users.noreply.github.com>
2024-01-31 15:26:37 +0100

    Update Gamealgorithm.py

--- Gamealgorithm.py
+++ Gamealgorithm.py
@@ -1,39 +1,44 @@
 from PlayingClasses import Deck, Table, cards
 from FINDONESET import findoneset
-from check_set import check_set, check_element
+from Check_Set import check_set, checkelement
 Mydeck=Deck()
-points=0
-computerpoints = 0
 Deck.shuffle(Mydeck)
 Mytable=Table()
 Table.newgame(Mytable,Mydeck)
-print(Mytable)
-index1,index2, index3 = input().split(",")
-def inputforcards(tableyourusing):
-    card1 = tableyourusing._table[int(index1)-1]
-    card2=tableyourusing._table[int(index2)-1]
-    card3=tableyourusing._table[int(index3)-1]
-    return card1,card2,card3
-cardv1,cardv2,cardv3=inputforcards(Mytable)
 
-
-
+def main(deck, table):
+    
+    while len(Mydeck) > 0:
+        
+        print(Mytable)
+        print(len(Mydeck))
+        index1,index2, index3 = input().split(",")
+        def inputforcards(tableyourusing):
+            card1 = tableyourusing._table[int(index1)-1]
+            card2=tableyourusing._table[int(index2)-1]
+            card3=tableyourusing._table[int(index3)-1]
+            return card1,card2,card3
+        cardv1,cardv2,cardv3=inputforcards(Mytable)
+        def whogetsapoint(deck, table, card1, card2, card3):
+            if check_set(cardv1,cardv2,cardv3)==True:
+                print("congrats you got a point!")
+                
+                Mytable.setisfound(Mydeck,cardv1,cardv2,cardv3)
+                return True
+            else:
+                if findoneset(Mytable) != []:
+                    
+                    print('the computer found a set, so the computer got a point')
+                    k=findoneset(Mytable)
+                    print(k)
+                    Mytable.setisfound(Mydeck,k[0],k[1],k[2])
+                    return False
+                else: 
+                    print("idd leeg")
+                    Mytable.setisnotfound(Mydeck)
+            print(len(Mydeck))    
+            return Mytable
+        
+        updatedtable = whogetsapoint(Mydeck, Mytable, cardv1, cardv2, cardv3)
    
-if check_set(cardv1,cardv2,cardv3)==True:
-    print("congrats you got a point!")
-    points+=1
-    Mytable.setisfound(Mydeck,cardv1,cardv2,cardv3)
-else:
-    #print("unfortunately that's not a set, try again")
-    if findoneset(Mytable) != []:
-        computerpoints += 1
-        print('the computer found a set, so the computer got a point')
-        k=findoneset(Mytable)
-        print(k)
-        Mytable.setisfound(Mydeck,k[0],k[1],k[2])
-    else: 
-        print("idd leeg")
-        Mytable.setisnotfound(Mydeck)
-
-print(Mytable) 
-
+    return Mytable
