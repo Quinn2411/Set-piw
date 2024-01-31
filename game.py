08a99b6cc824e85a089322704251c0fa9aa014a0
Quinn2411 <156076473+Quinn2411@users.noreply.github.com>
2024-01-31 23:34:10 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,61 +1,60 @@
-from PlayingClasses import Deck, Table, cards
-from FINDONESET import findoneset
-from Check_Set import check_set, checkelement
-#Defining variables for the main function
-Mydeck=Deck()
-Deck.shuffle(Mydeck)
-Mytable=Table()
-Table.newgame(Mytable,Mydeck)
+from models import Deck, Table, Card
+from find_one_set import find_one_set
+from check_set import check_set
 
-def main(deck, table):
-    points = 0
-    computerpoints = 0
-    #Letting the game repeat until all cards have been on the table once
-    while len(Mydeck) > 0:
-        print(Mytable)
-        print(len(Mydeck))
-        #Using Try and except to deal with incorrect inputs, so it doesn't crash the game
-        try:
-            index1,index2, index3 = input().split(",")
-        except:
-            print("invalid input, try again in the form a,b,c")
-            exit() 
-        #Putting the input in an index to make the code runs smoother and is more userfriendly
-        def inputforcards(tableyourusing):
-            try:
-                card1 = tableyourusing._table[int(index1)-1]
-                card2=tableyourusing._table[int(index2)-1]
-                card3=tableyourusing._table[int(index3)-1]
-                return card1,card2,card3
-            except: 
-                print("invalid input, try again in the form a,b,c")
-                exit()
+# defining variables for the main function
+my_deck = Deck()
+my_deck.shuffle()
 
-        cardv1,cardv2,cardv3=inputforcards(Mytable)
-        #Checking if the given cards form a set
+my_table = Table()
+my_table.new_game(my_deck)
+
+# putting the input in an index to make the code runs smoother and is more userfriendly
+def get_input_cards(table):
+    try:
+        index1, index2, index3 = input().split(",")
+    except KeyboardInterrupt:
+        exit()
+    except:
+        print("invalid input, try again in the form a,b,c")
+        return None, None, None
         
-        if check_set(cardv1,cardv2,cardv3)==True:
-            points += 1
-            print("congrats you got a point, the score is now ",points,computerpoints)
-            Mytable.setisfound([Mydeck,index1,index2,index3])
-        else:
-            if findoneset(Mytable) != None:
-                computerpoints += 1
-                print("the computer found a set, so the computer got a point, the score is now ",points, computerpoints)
-                k=findoneset(Mytable)
-                print(k)
-                
-                Mytable.setisfound(Mydeck,k[0],k[1],k[2])
-                #Failsafe incase of CAP_Set on table
-            else: 
-                print("idd leeg")
-                Mytable.setisnotfound(Mydeck)    
+    try:
+        card1 = table[int(index1)-1]
+        card2 = table[int(index2)-1]
+        card3 = table[int(index3)-1]
+        return card1, card2, card3
+    except: 
+        print("invalid input, try again in the form a,b,c")
+        return None, None, None
+
+points = 0
+computerpoints = 0
+
+# letting the game repeat until all cards have been on the table once
+while len(my_deck) > 0:
+    print(my_table)
+    print(len(my_deck))
+    
+    # using Try and except to deal with incorrect inputs, so it doesn't crash the game
+    card1, card2, card3 = get_input_cards(my_table)
+    if card1 == None:
+        continue
+    
+    # checking if the given cards form a set
+    if check_set(card1, card2, card3):
+        points += 1
+        print(f"congrats you got a point\n the score is now: computer: {computerpoints}, player: {points}")
+        my_table.remove_set(my_deck, card1, card2, card3)
+    else:
+        if find_one_set(my_table) != None:
+            computerpoints += 1
+            print(f"the computer found a set, so the computer got a point\n the score is now: computer: {computerpoints}, player: {points}")
+            card1, card2, card3 = find_one_set(my_table)
+            print(card1, card2, card3)
             
-          
-        
-    return Mytable
-
-print (main(Mydeck, Mytable))
-
- 
-
+            my_table.remove_set(my_deck, card1, card2, card3)
+        # failsafe incase of CAP_Set on table
+        else: 
+            print("there is no set")
+            my_table.refresh(my_deck)    
